044c3c934bb1e005943a83fc5d598841f59189c1
eliottvalette <eliott.valette@gmail.com>
2025-04-03 22:36:01 +0200

    Modification de la logique de chargement du modèle dans main.py pour activer le chargement conditionnel, ajustement du taux d'épsilon de départ dans config.py, et mise à jour de plusieurs images de visualisation. Désactivation du partage de modèle entre les agents IA.

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ for i in range(6):
         action_size=12,
         gamma=GAMMA,
         learning_rate=ALPHA,
-        load_model=False, # if i != 0 else True,
+        load_model=False if i >= 3 else True,
         load_path=f"saved_models/poker_agent_{list_names[i]}.pth",
         show_cards=True
     )
@@ -39,7 +39,7 @@ for agent in agent_list:
     print('agent :', agent.name)
 
 # Faire en sorte que les agents IA partagent le même modèle (Changer entre True et False pour activer ou désactiver le partage de modèle)
-if True:
+if False:
     shared_model = agent_list[0].model
     shared_memory = agent_list[0].memory
     for agent in agent_list:  # Seulement pour les 4 premiers agents qui sont des IA
